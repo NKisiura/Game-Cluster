30c3f35aa0becb6be1f049d2ec96d1c3a45c7a5a
Nikita Kisiura <nikitar156@gmail.com>
2022-06-29 23:30:17 +0300

    created api-constants

---
+++ src/app/global/constants/api-constants.ts
@@ -0,0 +1,14 @@
+// Key from https://rawg.io/ production requests
+// export const API_KEY = 'c542e67aec3a4340908f9de9e86038af';
+
+export const API_KEY = '8863de5e70b1474e8fb85d45ba2b5faa';
+export const API_BASE_URL = 'https://api.rawg.io/api';
+export const API_GAMES_URL = 'games';
+export const API_PLATFORMS_URL = 'platforms';
+export const API_GENRES_URL = 'genres';
+export const API_STORES_URL = 'stores';
+export const API_TAGS_URL = 'tags';
+export const API_PUBLISHERS_URL = 'publishers';
+export const API_DEVELOPERS_URL = 'developers';
+export const API_CREATORS_URL = 'creators';
+export const API_CREATOR_ROLE_URL = 'creator-roles';
