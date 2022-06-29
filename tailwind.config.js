3b120415d7d51f4a5afd96f461a9527bfcc37951
Nikita Kisiura <nikitar156@gmail.com>
2022-06-29 16:47:25 +0300

    added tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  darkMode: "class",
+  theme: {
+    container: {
+      center: true,
+      padding: "1rem",
+    },
+    colors: {},
+  },
+  plugins: [],
+};
