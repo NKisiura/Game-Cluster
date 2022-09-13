394fb5461c196c7f7ba68ff197bc9da3b1a66c61
Nikita Kisiura <nikitar156@gmail.com>
2022-09-13 17:05:26 +0300

    created card-list module

---
+++ src/app/global/modules/layouts/card-list/card-list.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CardListComponent } from './components/card-list/card-list.component';
+
+@NgModule({
+  declarations: [CardListComponent],
+  imports: [CommonModule],
+  exports: [CardListComponent],
+})
+export class CardListModule {}
