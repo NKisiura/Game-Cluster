e5a023151634ec99393d59615dc18073b921fb89
Nikita Kisiura <nikitar156@gmail.com>
2022-09-16 16:52:34 +0300

    created Language enum

--- src/app/global/types/entities/tags/tag.interface.ts
+++ src/app/global/types/entities/tags/tag.interface.ts
@@ -1,7 +1,12 @@
 import { Entity } from '../entity';
 
 export interface TagInterface extends Entity {
-  readonly language: string;
+  readonly language: Language;
   readonly games_count: number;
   readonly image_background: string;
+}
+
+export enum Language {
+  ENGLISH = 'eng',
+  RUSSIAN = 'rus',
 }
