d4a1d4843bb035462b0eabcd26ef3cafc3559a47
Nikita Kisiura <nikitar156@gmail.com>
2022-09-16 11:52:26 +0300

    get cropped images in sidebar

--- src/app/modules/sidebar/components/sidebar-entity-list/sidebar-entity-list.component.ts
+++ src/app/modules/sidebar/components/sidebar-entity-list/sidebar-entity-list.component.ts
@@ -5,6 +5,7 @@ import { Params } from '@angular/router';
 import { EntityTypes } from '../../../../global/types/entities/entity-types.enum';
 import { IconService } from '../../../../global/utils/services/icon.service';
 import { IconDefinition } from '@fortawesome/free-brands-svg-icons';
+import { ImageService } from '../../../../global/utils/services/image.service';
 
 @Component({
   selector: 'app-sidebar-entity-list',
@@ -18,7 +19,10 @@ export class SidebarEntityListComponent implements OnInit {
   public gamesRouterLink: string = RouterLinks.GAMES_ROUTER_LINK;
   public isCurrentEntityGenre!: boolean;
 
-  constructor(private iconService: IconService) {}
+  constructor(
+    private iconService: IconService,
+    private imageService: ImageService
+  ) {}
 
   ngOnInit(): void {
     this.isCurrentEntityGenre = EntityTypes.GENRES === this.entityType;
@@ -30,6 +34,10 @@ export class SidebarEntityListComponent implements OnInit {
 
   public getIconBySlug(slug: string): IconDefinition {
     return this.iconService.getIconBySlug(slug);
+  }
+
+  public getCroppedImage(imageUrl: string) {
+    return this.imageService.getCroppedImage600x400(imageUrl);
   }
 }
 
