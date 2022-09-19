9ddb8953dd4ceeba7e0517a8a6b7288163f155dd
Nikita Kisiura <nikitar156@gmail.com>
2022-09-19 22:54:30 +0300

    dispatch initial actions in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,34 @@
-import { AfterViewChecked, Component, ViewChild } from '@angular/core';
+import { AfterViewChecked, Component, OnInit, ViewChild } from '@angular/core';
 import { map, skip, Subject, takeUntil } from 'rxjs';
 import { NgProgressComponent } from 'ngx-progressbar';
+import { PlatformsActions } from './state/features/platforms/actions/platforms.actions';
+import {
+  API_GENRES_URL,
+  API_PLATFORMS_URL,
+  API_STORES_URL,
+} from './global/constants/api-constants';
+import { GenresActions } from './state/features/genres/actions/genres.actions';
+import { StoresActions } from './state/features/stores/actions/stores.actions';
+import { Store } from '@ngrx/store';
+import { AppStateInterface } from './state/types/app-state.interface';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements AfterViewChecked {
+export class AppComponent implements OnInit, AfterViewChecked {
   @ViewChild('progressComponent')
   public progressComponent!: NgProgressComponent;
   public isFirstInit: boolean = true;
   public appLoading: boolean = true;
   private appLoadingEnd$: Subject<void> = new Subject<void>();
+
+  constructor(private readonly store$: Store<AppStateInterface>) {}
+
+  ngOnInit(): void {
+    this.dispatchInitialActions();
+  }
 
   ngAfterViewChecked(): void {
     if (this.isFirstInit) {
@@ -33,5 +49,13 @@ export class AppComponent implements AfterViewChecked {
       this.appLoadingEnd$.next();
       this.appLoadingEnd$.complete();
     }
+  }
+
+  private dispatchInitialActions(): void {
+    this.store$.dispatch(
+      PlatformsActions.getPlatforms({ url: API_PLATFORMS_URL })
+    );
+    this.store$.dispatch(GenresActions.getGenres({ url: API_GENRES_URL }));
+    this.store$.dispatch(StoresActions.getStores({ url: API_STORES_URL }));
   }
 }
--- src/app/modules/sidebar/components/sidebar/sidebar.component.ts
+++ src/app/modules/sidebar/components/sidebar/sidebar.component.ts
@@ -4,17 +4,9 @@ import { AppStateInterface } from '../../../../state/types/app-state.interface';
 import { filter, map, Observable } from 'rxjs';
 import { PlatformInterface } from '../../../../global/types/entities/platforms/platform.interface';
 import { PlatformsSelectors } from '../../../../state/features/platforms/selectors/platforms.selectors';
-import { PlatformsActions } from '../../../../state/features/platforms/actions/platforms.actions';
-import {
-  API_GENRES_URL,
-  API_PLATFORMS_URL,
-  API_STORES_URL,
-} from '../../../../global/constants/api-constants';
 import { BackendErrorResponseInterface } from '../../../../state/types/backend-error-response.interface';
 import { GenreInterface } from '../../../../global/types/entities/genres/genre.interface';
 import { GenresSelectors } from '../../../../state/features/genres/selectors/genres.selectors';
-import { GenresActions } from '../../../../state/features/genres/actions/genres.actions';
-import { StoresActions } from '../../../../state/features/stores/actions/stores.actions';
 import { StoreInterface } from '../../../../global/types/entities/stores/store.interface';
 import { StoresSelectors } from '../../../../state/features/stores/selectors/stores.selectors';
 import { MainEntitiesService } from '../../../../global/utils/services/main-entities.service';
@@ -48,16 +40,7 @@ export class SidebarComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.initActions();
     this.initValues();
-  }
-
-  private initActions(): void {
-    this.store$.dispatch(
-      PlatformsActions.getPlatforms({ url: API_PLATFORMS_URL })
-    );
-    this.store$.dispatch(GenresActions.getGenres({ url: API_GENRES_URL }));
-    this.store$.dispatch(StoresActions.getStores({ url: API_STORES_URL }));
   }
 
   private initValues(): void {
