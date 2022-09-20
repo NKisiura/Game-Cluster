40786ef533f7379d7a9b504145cebd67460c2883
Nikita Kisiura <nikitar156@gmail.com>
2022-09-20 19:02:43 +0300

    small fix

--- src/app/state/types/app-state.interface.ts
+++ src/app/state/types/app-state.interface.ts
@@ -2,10 +2,12 @@ import { PlatformsStateInterface } from '../features/platforms/types/platforms-s
 import { GamesStateInterface } from '../features/games/types/games-state.interface';
 import { GameDetailsStateInterface } from '../features/game-details/types/game-details-state.interface';
 import { StoresStateInterface } from '../features/stores/types/stores-state.interface';
+import { TagsStateInterface } from '../features/tags/types/tags-state.interface';
 
 export interface AppStateInterface {
   readonly platforms: PlatformsStateInterface;
   readonly games: GamesStateInterface;
   readonly gameDetails: GameDetailsStateInterface;
   readonly stores: StoresStateInterface;
+  readonly tags: TagsStateInterface;
 }
