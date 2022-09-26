2eac215d599c371c9d0d7933bbbb82598c137533
Nikita Kisiura <nikitar156@gmail.com>
2022-09-26 17:02:50 +0300

    styled page not found

--- src/app/global/modules/layouts/page-not-found/components/page-not-found/page-not-found.component.ts
+++ src/app/global/modules/layouts/page-not-found/components/page-not-found/page-not-found.component.ts
@@ -1,7 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLinks } from '../../../../../constants/router-links';
 
 @Component({
   selector: 'app-page-not-found',
   templateUrl: './page-not-found.component.html',
+  styleUrls: ['./page-not-found.component.scss'],
 })
-export class PageNotFoundComponent {}
+export class PageNotFoundComponent {
+  public baseRouterLink: string = RouterLinks.BASE_ROUTE;
+}
