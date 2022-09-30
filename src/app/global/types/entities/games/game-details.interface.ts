488217893bf1836ed0c8f1c57ebc06d3ce73f868
Nikita Kisiura <nikitar156@gmail.com>
2022-09-30 22:00:57 +0300

    changed game details interface

--- src/app/global/types/entities/games/game-details.interface.ts
+++ src/app/global/types/entities/games/game-details.interface.ts
@@ -2,4 +2,6 @@ import { GameInterface } from './game.interface';
 
 export interface GameDetailsInterface extends GameInterface {
   readonly description: string;
+  readonly description_raw: string;
+  readonly playtime: number;
 }
