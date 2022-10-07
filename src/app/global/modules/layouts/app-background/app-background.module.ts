6ce135b38c07428492968765de6db01b72acb43c
Nikita Kisiura <nikitar156@gmail.com>
2022-10-07 14:16:44 +0300

    created app background showHide animation

--- src/app/global/modules/layouts/app-background/app-background.module.ts
+++ src/app/global/modules/layouts/app-background/app-background.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AppBackgroundComponent } from './components/app-background/app-background.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppBackgroundComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, BrowserAnimationsModule],
   exports: [AppBackgroundComponent],
 })
 export class AppBackgroundModule {}
--- src/app/global/modules/layouts/app-background/components/app-background/app-background.component.ts
+++ src/app/global/modules/layouts/app-background/components/app-background/app-background.component.ts
@@ -3,11 +3,24 @@ import { select, Store } from '@ngrx/store';
 import { RootStateInterface } from '../../../../../../state/types/root-state.interface';
 import { GameDetailsSelectors } from '../../../../../../state/features/game-details/selectors/game-details.selectors';
 import { Observable } from 'rxjs';
+import { animate, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-background',
   templateUrl: './app-background.component.html',
   styleUrls: ['./app-background.component.scss'],
+  animations: [
+    trigger('showHide', [
+      transition(':enter', [
+        style({ opacity: '0' }),
+        animate('200ms', style({ opacity: '*' })),
+      ]),
+      transition(':leave', [
+        style({ opacity: '*' }),
+        animate('200ms', style({ opacity: '0' })),
+      ]),
+    ]),
+  ],
 })
 export class AppBackgroundComponent implements OnInit {
   public appBackgroundImageUrl$ = new Observable<null | string>();
