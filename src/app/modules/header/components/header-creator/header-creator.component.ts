ba3fd8c5b9e737689031588ee7b92469ff46b8c0
Nikita Kisiura <nikitar156@gmail.com>
2022-10-12 14:27:12 +0300

    changed header creator component

--- src/app/modules/header/components/header-creator/header-creator.component.ts
+++ src/app/modules/header/components/header-creator/header-creator.component.ts
@@ -3,5 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header-creator',
   templateUrl: './header-creator.component.html',
+  styleUrls: ['./header-creator.component.scss'],
 })
-export class HeaderCreatorComponent {}
+export class HeaderCreatorComponent {
+  public gitHubLink: string = '';
+}
